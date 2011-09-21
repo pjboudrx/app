253a2108aac3af2f9f6b192f1da6a0b0e0fbdfa8
Patrick Boudreaux <pboudreaux@daxko.com>
2011-09-21 10:17:43 -0500

    central stub gateway with concrete type

---
+++ source/nothinbutdotnetstore.specs/StubSpecs.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using nothinbutdotnetstore.web.application.catalogbrowsing.stubs;
+using nothinbutdotnetstore.web.core;
+using developwithpassion.specifications.extensions;
+
+namespace nothinbutdotnetstore.specs
+{
+    [Subject(typeof(Stub))]
+    public class StubSpecs
+    {
+		public abstract class concern : Observes<Stub>
+        {
+        }
+
+        public class when_creating_a_stub : concern
+        {
+			public class and_the_concrete_type_is_known : concern
+			{
+				Establish c = () =>
+				{
+					stub = Stub.with<StubDepartmentRepository>();
+				};
+
+				private It should_create_an_instance_of_the_specified_type = () =>
+											stub.ShouldBeAn<StubDepartmentRepository>().
+											ShouldNotBeNull();
+
+				private static object stub;
+			}
+        }
+    }
+}
---
+++ source/nothinbutdotnetstore/web/application/catalogbrowsing/stubs/Stub.cs
@@ -0,0 +1,11 @@
+﻿namespace nothinbutdotnetstore.web.application.catalogbrowsing.stubs
+{
+	public class Stub
+	{
+		public static TypeOfStub with<TypeOfStub>() 
+			where TypeOfStub : new()
+		{
+			return new TypeOfStub();
+		}
+	}
+}
