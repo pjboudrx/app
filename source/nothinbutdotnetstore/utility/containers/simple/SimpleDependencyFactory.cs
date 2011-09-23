48241d8a508052e89e243f82ccac7947c8d7fe3f
Patrick Boudreaux <pboudreaux@daxko.com>
2011-09-23 09:43:08 -0500

    Pushing new changes

--- source/nothinbutdotnetstore/utility/containers/simple/SimpleDependencyFactory.cs
+++ source/nothinbutdotnetstore/utility/containers/simple/SimpleDependencyFactory.cs
@@ -1,10 +1,19 @@
-﻿namespace nothinbutdotnetstore.utility.containers.simple
+﻿using System;
+
+namespace nothinbutdotnetstore.utility.containers.simple
 {
     public class SimpleDependencyFactory : ICreateADependency
     {
-        public object create()
-        {
-            throw new System.NotImplementedException();
-        }
+    	private readonly Func<object> _creator;
+
+    	public SimpleDependencyFactory(Func<object> creator)
+    	{
+    		_creator = creator;
+    	}
+
+    	public object create()
+    	{
+    		return _creator();
+    	}
     }
 }
